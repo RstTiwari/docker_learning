4b174bb5467b19b8230910abc77f115843ff48b2
root <root@DESKTOP-K7I4LM9.localdomain>
2026-07-04 15:57:40 +0530

    added v8

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     res.json({
-        msg: "hi i am responding from the server v7",
+        msg: "hi i am responding from the server v8",
     });
 });
 
